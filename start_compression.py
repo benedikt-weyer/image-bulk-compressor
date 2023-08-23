13ae41df59627520a95e7736f8b07cbf947241b8
MasterOC <benedikt.oskar.weyer@gmail.com>
2023-08-23 15:46:56 +0200

    implemented file loading

--- start_compression.py
+++ start_compression.py
@@ -1,2 +1,25 @@
 from PIL import Image
 import os
+
+#set paths
+raw_images_path = 'raw-images'
+compressed_images_path = 'compressed-images'
+
+raw_images = []
+
+valid_file_extensions = ['.jpg', '.jpeg',]
+
+#load all images from input dir
+for file_name in os.listdir(raw_images_path):
+    file_extention = os.path.splitext(file_name)[1]
+    
+    #check vor valid file extension
+    if file_extention.lower() not in valid_file_extensions:
+        print(f'[WARN] File "{file_name}" not allowed because of not supported file extension. Skipping..')
+        continue
+
+    #load image and save in array
+    raw_images.append(Image.open(os.path.join(raw_images_path, file_name)))
+
+    print(f'[INFO] "{file_name}" loaded successfully!')
+   
