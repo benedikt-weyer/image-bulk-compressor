7cb32c6eb1403ea97cdd1f903a14734598b26c42
MasterOC <benedikt.oskar.weyer@gmail.com>
2023-08-29 17:51:15 +0200

    added image quality prompt + compression

--- start_compression.py
+++ start_compression.py
@@ -22,4 +22,30 @@ for file_name in os.listdir(raw_images_path):
     raw_images.append(Image.open(os.path.join(raw_images_path, file_name)))
 
     print(f'[INFO] "{file_name}" loaded successfully!')
-   
+
+print(f'[INFO] All images loaded sucessfully!')
+
+
+valid_input = False
+image_quality = 100
+
+while not valid_input:
+    print(f'[PROMPT] Please enter new image quality between 0 and 100: ')
+
+    #get image quality from console
+    input_image_quality = input()
+
+    #convert to int
+    image_quality = int(input_image_quality)
+
+    #validate input
+    valid_input = image_quality > 0 and image_quality < 100
+
+
+#compress and save loaded images
+for raw_image in raw_images:
+    raw_image.save(os.path.join(compressed_images_path, os.path.basename(raw_image.filename)), 'jpeg', optimize = True, quality = image_quality)
+
+    print(f'[INFO] "{os.path.basename(raw_image.filename)}" compressed successfully!')
+
+print(f'[INFO] Done!')
